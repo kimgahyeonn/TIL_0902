210958bf335d1a08e3dd44408ce768c46d21ddc9
kimgahyeonn <in47821@gmail.com>
2024-09-02 17:45:52 +0900

    17:45

--- 05_완전검색_그리디/5188_최소합/sol.py
+++ 05_완전검색_그리디/5188_최소합/sol.py
@@ -1,2 +1,17 @@
 import sys
 sys.stdin = open('input.txt')
+T = int(input())
+
+def bfs(start) :
+
+
+
+
+for tc in range(1, T+1) :
+    N = int(input())
+    arr = [list(map(int, input().split())) for _ in range(N)]
+
+    queue = [[0] * N for _ in range(N)]
+    visited = [[0] * N for _ in range(N)]
+
+    bfs(1)
